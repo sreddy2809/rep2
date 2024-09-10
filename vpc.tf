352eb06639405c71359d831d110e1569451491f1
Sheshi Reddy <sheshi@hsbc.com>
2024-09-10 08:41:36 +0530

    Changes Done for CR-101

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,12 @@
 variable "x"{
 	type=map
 }
+variable "Y"{
+	type=list
+	default=["10.0.0.0/24","10.1.0.0/24"]
+}
 resource "aws_vpc" "vpc1"{
+	count=length(var.y)
 	cidr_block="10.0.0.0/24"
 }
 resource "aws_subnet" "sn1"{
