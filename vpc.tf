15fe91ea3954c31e908914dd739e66eda1804483
harsha vardhan <harsha@hsbc.com>
2024-09-10 03:15:19 +0000

    Done Changes for CR-102

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,14 @@
 variable "x"{
 	type=map
 }
+variable "vpc_cidr"{
+	default="192.168.0.0/24"
+}
 resource "aws_vpc" "vpc1"{
-	cidr_block="10.0.0.0/24"
+	cidr_block=var.vpc_cidr
 }
 resource "aws_subnet" "sn1"{
-	cidr_block="10.0.0.0/24"
+	cidr_block=var.vpc_cidr
 	vpc_id=aws_vpc.vpc1.id
 	tags={
 		Name="MBAPP"
